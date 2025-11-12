3734460971802d3a0308b86f56b1c7e253c8750d
Italo <4081360@banesecard.com>
2025-11-12 10:47:05 -0300

    Consertando erros de Davi e Vitor Hugo

--- frontend/jotanunes-front/src/views/pages/index/Index.jsx
+++ frontend/jotanunes-front/src/views/pages/index/Index.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom'
 import {
   CButton,
+  CCardTitle,
+  CCardText,
   CForm,
   CFormSelect,
   CHeader,
--- frontend/jotanunes-front/src/views/pages/projeto/Projeto.jsx
+++ frontend/jotanunes-front/src/views/pages/projeto/Projeto.jsx
@@ -107,7 +107,7 @@ const Projeto = () => {
 
           // 2. Define os estados com os dados do template
           setPrefacioData(templateData.prefacioData || { nome: '', estado: '', cidade: '', texto: '' });
-          alert(unidadesData);
+          // alert(unidadesData);
           setUnidadesData(templateData.unidadesData || []);
           setAreacomumData(templateData.areacomumData || []);
           setMaterialData(templateData.materialData || []);
